8d7123d11ce2d9f4e952f23ec7f27e9f55ca04cc
Maksim Timofeyev <makstim@list.ru>
2024-06-14 16:26:28 +0300

    Initial commit

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -4,10 +4,6 @@ package ru.netology
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 fun main() {
-    val name = "Kotlin"
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    println("Hello, " + name + "!")
 
     ChatService.createMessage(11, "this is message from owner #11 number 1")
     ChatService.createMessage(22, "this is message from owner #22 number 1")
