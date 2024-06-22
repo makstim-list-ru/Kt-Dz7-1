7f32a2aca6d935b7eba13c3d8fd87a38c6a2e724
Maksim Timofeyev <makstim@list.ru>
2024-06-22 17:07:01 +0300

    Commit w/tests corrected1

--- src/test/kotlin/ChatServiceTest.kt
+++ src/test/kotlin/ChatServiceTest.kt
@@ -4,6 +4,7 @@ import org.junit.Test
 
 import org.junit.Assert.*
 import org.junit.Before
+import ru.netology.ChatService.chatList
 
 class ChatServiceTest {
 
@@ -21,39 +22,55 @@ class ChatServiceTest {
     fun getUnreadChatsCount() {
 //        println("<<getUnreadChatsCount()>>\n"+ChatService.getUnreadChatsCount())
 //        println()
-        assertEquals(2,ChatService.getUnreadChatsCount())
+        assertEquals(2, ChatService.getUnreadChatsCount())
     }
 
     @Test
     fun getChats() {
-        println("<<getChats()>>\n"+ChatService.getChats())
-        println()
+        assertEquals(2, ChatService.getChats().size)
+//        println("<<getChats()>>\n" + ChatService.getChats())
+//        println()
     }
 
     @Test
     fun getLastMessagesFromChats() {
-        println("<<getLastMessagesFromChats()>>\n"+ChatService.getLastMessagesFromChats())
-        println()
+        assertEquals(2, ChatService.getLastMessagesFromChats().size)
+//        println("<<getLastMessagesFromChats()>>\n" + ChatService.getLastMessagesFromChats())
+//        println()
     }
 
     @Test
     fun getMessagesFromChat() {
-        println("<<getMessagesFromChat(22,2)>>\n"+ChatService.getMessagesFromChat(22,2))
-        println()
+
+        val tmpStringList = ChatService.getMessagesFromChat(22, 2)
+
+        assertEquals("this is message from owner #22 number 2", tmpStringList[0])
+        assertEquals("this is message from owner #22 number 3", tmpStringList[1])
+
+//        println("<<getMessagesFromChat(22,2)>>\n" + ChatService.getMessagesFromChat(22, 2))
+//        println()
     }
 
     @Test(expected = RuntimeException::class)
     fun getMessagesFromChatRtE() {
-        println("<<getMessagesFromChat(33,2)>>\n"+ChatService.getMessagesFromChat(33,2))
+        println("<<getMessagesFromChat(33,2)>>\n" + ChatService.getMessagesFromChat(33, 2))
         println()
     }
 
     @Test
     fun deleteMessage() {
-        println("<<deleteMessage(11, this is message from owner #11 number 2) - BEFORE>>\n"+ChatService.getChats())
+
+        val chatListBefore =
+            chatList.filter { chat: Chat -> chat.messages.contains("this is message from owner #11 number 2") }
+//        println("<<deleteMessage(11, this is message from owner #11 number 2) - BEFORE>>\n"+ChatService.getChats())
         ChatService.deleteMessage(11, "this is message from owner #11 number 2")
-        println("<<deleteMessage(11, this is message from owner #11 number 2) - AFTER>>\n"+ChatService.getChats())
-        println()
+//        println("<<deleteMessage(11, this is message from owner #11 number 2) - AFTER>>\n"+ChatService.getChats())
+//        println()
+        val chatListAfter =
+            chatList.filter { chat: Chat -> chat.messages.contains("this is message from owner #11 number 2") }
+
+        assertEquals(1, chatListBefore.size)
+        assertEquals(0, chatListAfter.size)
     }
 
     @Test(expected = RuntimeException::class)
@@ -64,9 +81,15 @@ class ChatServiceTest {
 
     @Test
     fun deleteChat() {
+        val chatListBefore =
+            chatList.filter { chat: Chat -> chat.idOwner == 22 }
         ChatService.deleteChat(22)
-        println("<<deleteChat(22)>>\n"+ChatService.getChats())
-        println()
+        val chatListAfter =
+            chatList.filter { chat: Chat -> chat.idOwner == 22 }
+        assertEquals(1, chatListBefore.size)
+        assertEquals(0, chatListAfter.size)
+//        println("<<deleteChat(22)>>\n" + ChatService.getChats())
+//        println()
     }
 
     @Test(expected = RuntimeException::class)
